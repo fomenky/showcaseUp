eb2cc4df97baf9453972bb452b3bee3862a70d71
AceGod <acegod@acegods-mac.local>
2016-08-31 21:47:26 -0700

    Initial Commit -- Added custom class styles for Views, Buttons and textFields

---
+++ showcaseUp/MaterialView.swift
@@ -0,0 +1,16 @@
+//
+//  MaterialView.swift
+//  showcaseUp
+//
+//  Created by AceGod on 8/31/16.
+//  Copyright © 2016 AceGod. All rights reserved.
+//  Used to style my views
+
+import UIKit
+
+class MaterialView: UIView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
