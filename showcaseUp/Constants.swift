dc4063c6889227af49b8394ae3f38186b1d38a20
AceGod <acegod@acegods-mac.local>
2016-08-31 21:48:33 -0700

    Initial Commit -- Added global constant SHADOW_COLOR

---
+++ showcaseUp/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  showcaseUp
+//
+//  Created by AceGod on 8/31/16.
+//  Copyright © 2016 AceGod. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0 // Must divide by 255 to get a value between 0-1
+
+
+
+
+
