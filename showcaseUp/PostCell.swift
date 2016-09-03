a8df7292e79db01ad5513cc8ddd4213551b2061a
AceGod <acegod@acegods-mac.local>
2016-09-02 18:05:20 -0700

    Third Commit -- Added PostCell UITableViewCell

---
+++ showcaseUp/PostCell.swift
@@ -0,0 +1,31 @@
+//
+//  PostCell.swift
+//  showcaseUp
+//
+//  Created by AceGod on 9/2/16.
+//  Copyright © 2016 AceGod. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var showcaseImg: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func drawRect(rect: CGRect) {
+        profileImg.layer.cornerRadius = profileImg.frame.size.width / 2
+        profileImg.clipsToBounds = true
+        
+        showcaseImg.clipsToBounds = true
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: true)
+    }
+}
