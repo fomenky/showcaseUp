8c8fd40187a9ee658f6715a90b04a6543a4a17a3
AceGod <acegod@acegods-mac.local>
2016-09-02 17:59:36 -0700

    Third Commit -- Added RE_USERS / REF_POSTS

--- showcaseUp/DataService.swift
+++ showcaseUp/DataService.swift
@@ -9,14 +9,32 @@
 import Foundation
 import Firebase
 
+let BASE_URL: String = "showcase-myapps.firebaseio.com"
+
 class DataService{
-    let BASE_URL: String = "showcase-myapps.firebaseio.com"
 
     static let instance = DataService()
     
     private var _REF_BASE = FIRDatabase.database().reference()
+    private var _REF_USERS = FIRDatabase.database().referenceFromURL("\(BASE_URL)/users")
+    private var _REF_POSTS = FIRDatabase.database().referenceFromURL("\(BASE_URL)/posts")
     
     var REF_BASE: FIRDatabaseReference{
         return _REF_BASE
+    }
+    
+    var REF_USERS: FIRDatabaseReference{
+        return _REF_USERS
+    }
+    
+    var REF_POSTS: FIRDatabaseReference{
+        return _REF_POSTS
+    }
+    
+    func createFirebaseUser(uid: String, user: Dictionary<String, String>) {
+        //Here is gets the uid from a specific user
+        //However if there isn't a uid it will create one
+        
+        REF_USERS.child(uid).setValue(user)
     }
 }
