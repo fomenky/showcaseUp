e6aad4109e16a8e3fa95df3e6535cca22916a34a
AceGod <acegod@acegods-mac.local>
2016-09-03 08:31:43 -0700

    Third Commit -- Added FeedVC for user post/ created static tableview cell

---
+++ showcaseUp/FeedVC.swift
@@ -0,0 +1,37 @@
+//
+//  FeedVC.swift
+//  showcaseUp
+//
+//  Created by AceGod on 9/2/16.
+//  Copyright © 2016 AceGod. All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
+    }
+    
+    
+    
+    
+    
+}
