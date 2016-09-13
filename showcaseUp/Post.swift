a39479f8da5d1ba360157d281b300f31171e80b9
AceGod <acegod@acegods-mac.local>
2016-09-12 21:47:38 -0700

    Third commit -- Added Post File to store data requested from Firebase

---
+++ showcaseUp/Post.swift
@@ -0,0 +1,61 @@
+//
+//  Post.swift
+//  showcaseUp
+//
+//  Created by AceGod on 9/12/16.
+//  Copyright © 2016 AceGod. All rights reserved.
+//
+
+import Foundation
+
+class Post  {
+    private var _postDescription: String!
+    private var _imageUrl: String?
+    private var _likes: Int!
+    private var _username: String!
+    private var _postKey: String! // Saving the uid just in case we may need it later
+    
+    var postDescription: String {
+        return _postDescription
+    }
+    
+    var imageUrl: String? {
+        return _imageUrl
+    }
+    
+    var likes: Int {
+        return _likes
+    }
+    
+    var username: String {
+        return _username
+    }
+    
+    var postKey: String{
+        return _postKey
+    }
+    
+    
+    init(description: String, imageUrl: String?, username: String){
+        self._postDescription = description
+        self._imageUrl = imageUrl
+        self._username = username
+    }
+    
+    init(postKey: String, dictionary: Dictionary<String, AnyObject>){
+        self._postKey = postKey
+        
+        if let likes = dictionary["likes"] as? Int{
+            self._likes = likes
+        }
+        
+        if let imgUrl = dictionary["imageUrl"] as? String {
+            self._imageUrl = imgUrl
+        }
+        
+        if let desc = dictionary["description"] as? String{
+            self._postDescription = desc
+        }
+    }
+    
+}
